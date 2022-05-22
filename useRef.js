ec7d632d24a98a6130fb483053cfc91246cd00e5
Ali Imran <aimran@ssidecisions.com>
2022-05-23 04:07:01 +0500

    Updated useRef file

--- useRef.js
+++ useRef.js
@@ -44,3 +44,36 @@ function TextInputWithFocusButton() {
 //   In this example, a parent component that renders <FancyInput ref={inputRef} /> 
 //   would be able to call inputRef.current.focus().
 
+
+// The useRef Hook allows you to persist values between renders.
+
+// It can be used to store a mutable value that does not cause a re-render when updated.
+
+// It can be used to access a DOM element directly.
+
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
