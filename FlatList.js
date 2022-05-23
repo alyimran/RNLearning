ddbef5494642114489ff0ca887dba6c683124e6e
Ali Imran <aimran@ssidecisions.com>
2022-05-23 05:41:16 +0500

    added custom  flatlist  file

---
+++ FlatList.js
@@ -0,0 +1,3 @@
+// Keys help React identify which items have changed, are added, 
+// or are removed. Keys should be given to the elements inside the array to give the
+//  elements a stable identity:
--- customHooks.js
+++ customHooks.js
@@ -1,6 +1,10 @@
 // A custom Hook is a JavaScript function whose name starts with ”use” and that may call other Hooks.
 // When we want to share logic between two JavaScript functions, we extract it to a third
 //  function. Both components and Hooks are functions, so this works for them too!
+// Hooks are reusable functions.
+
+// When you have component logic that needs to be used by multiple components, 
+// we can extract that logic to a custom Hook.
 
 
 import { useState, useEffect } from 'react';
@@ -31,3 +35,22 @@ function FriendStatus(props) {
     }
     return isOnline ? 'Online' : 'Offline';
   }
+
+
+
+  import { useContext } from "react";
+import { Context as TrackContext } from "../context/TrackContext";
+import { Context as LocationContext } from "../context/LocationContext";
+import {navigate} from '../navigatorRef'
+
+export default () => {
+    const {createTracks} = useContext(TrackContext)
+    const {state:{name, locations} , reset} = useContext(LocationContext)
+
+    const saveTrack = async()=>{
+       const result =  await createTracks(name, locations);
+       reset();
+       navigate("TrackList")
+    }
+    return [saveTrack];
+}
