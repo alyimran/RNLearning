7d326a11e93090f563bb7acee4a38498a7977721
Ali Imran <aimran@ssidecisions.com>
2022-06-08 11:08:29 +0500

    Added example for render props

--- RenderProp.js
+++ RenderProp.js
@@ -1,2 +1,64 @@
 The term render prop refers to a technique for sharing code between react components using a
 prop whose value is function.
+
+A component with a render prop takes a function that returns a React element and calls it 
+instead of implementing its own render logic.
+
+<DataProvider render={data => (
+    <h1>Hello {data.target}</h1>
+  )}/>
+
+Libraries that use render props include React Router, Downshift and Formik.
+
+Example:
+rop that it uses to dynamically determine what to render–a render prop.
+
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
+    );
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+
+        {/*
+          Instead of providing a static representation of what <Mouse> renders,
+          use the `render` prop to dynamically determine what to render.
+        */}
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the mouse around!</h1>
+        <Mouse render={mouse => (
+          <Cat mouse={mouse} />
+        )}/>
+      </div>
+    );
+  }
+}
