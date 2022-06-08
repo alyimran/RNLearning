9685f3b484a5ac8a8c431a9e6484ad0ad30c9881
Ali Imran <aimran@ssidecisions.com>
2022-06-08 11:24:17 +0500

    Added example for render children

--- RenderProp.js
+++ RenderProp.js
@@ -62,3 +62,57 @@ class MouseTracker extends React.Component {
     );
   }
 }
+
+
+// Render children example
+class Cat extends React.Component {
+    render() {
+      const mouse = this.props.mouse;
+      return (
+        <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
+      );
+    }
+  }
+  
+  class Mouse extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleMouseMove = this.handleMouseMove.bind(this);
+      this.state = { x: 0, y: 0 };
+    }
+  
+    handleMouseMove(event) {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      });
+    }
+  
+    render() {
+      return (
+        <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+  
+          {/*
+            Instead of providing a static representation of what <Mouse> renders,
+            use the `render` prop to dynamically determine what to render.
+          */}
+          {this.props.children(this.state)}
+        </div>
+      );
+    }
+  }
+  
+  class MouseTracker extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Move the mouse around!</h1>
+          <Mouse>
+          {mouse => (
+            <Cat mouse={mouse} />
+          )}
+          </Mouse>
+        </div>
+      );
+    }
+  }
